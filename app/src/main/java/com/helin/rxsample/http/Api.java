9bc811c5e430301696267e3ebd880f19426023eb
helin <991293917@qq.com>
2016-11-14 14:49:23 +0800

    增加请求通用参数！

--- app/src/main/java/com/helin/rxsample/http/Api.java
+++ app/src/main/java/com/helin/rxsample/http/Api.java
@@ -4,6 +4,7 @@ package com.helin.rxsample.http;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -27,17 +28,40 @@ public class Api {
             //手动创建一个OkHttpClient并设置超时时间
             OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
             httpClientBuilder.connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+
             /**
-             * 对所有请求添加请求头
+             *  拦截器
              */
             httpClientBuilder.addInterceptor(new Interceptor() {
                 @Override
                 public okhttp3.Response intercept(Chain chain) throws IOException {
                     Request request = chain.request();
-                    okhttp3.Response originalResponse = chain.proceed(request);
-                    return originalResponse.newBuilder().header("mobileFlag", "adfsaeefe").addHeader("type", "4").build();
+
+//                    Request.Builder requestBuilder = request.newBuilder();
+//                    RequestBody formBody = new FormBody.Builder()
+//                            .add("1","2")
+//                            .build();
+
+                    HttpUrl.Builder authorizedUrlBuilder = request.url()
+                            .newBuilder()
+                            //添加统一参数 如手机唯一标识符,token等
+                            .addQueryParameter("key1","value1")
+                            .addQueryParameter("key2", "value2");
+
+                    Request newRequest = request.newBuilder()
+                            //对所有请求添加请求头
+                            .header("mobileFlag", "adfsaeefe").addHeader("type", "4")
+                            .method(request.method(), request.body())
+                            .url(authorizedUrlBuilder.build())
+                            .build();
+
+//                    okhttp3.Response originalResponse = chain.proceed(request);
+//                    return originalResponse.newBuilder().header("mobileFlag", "adfsaeefe").addHeader("type", "4").build();
+                    return  chain.proceed(newRequest);
                 }
             });
+
+
             SERVICE = new Retrofit.Builder()
                     .client(httpClientBuilder.build())
                     .addConverterFactory(GsonConverterFactory.create())
